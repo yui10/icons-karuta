09f90fdef2657d8a05d5ea46a883f973df72ec13
yui10 <79691301+yui10@users.noreply.github.com>
2024-03-31 16:39:43 +0900

    Delete console.log

--- src/utils/icon.ts
+++ src/utils/icon.ts
@@ -2,11 +2,8 @@ import * as simpleIcons from 'simple-icons';
 
 export const getIcon = (slug: string) => {
     if (!slug) return null;
-    console.log(slug);
     const replace_slug = slug.toLowerCase().replaceAll(' ', 'plus').replaceAll('+', 'plus').replaceAll('.', 'dot');
     const iconKeys = 'si' + replace_slug.charAt(0).toUpperCase() + replace_slug.slice(1);
-    console.log(iconKeys);
-    console.log(iconKeys in simpleIcons);
     if (iconKeys in simpleIcons) {
         return (simpleIcons as any)[iconKeys];
     }
