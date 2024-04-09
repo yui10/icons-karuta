9dea72d29b3df7c9c1e62df1a921272bfe2d2cc3
yui10 <79691301+yui10@users.noreply.github.com>
2024-04-09 16:39:46 +0900

    ゲームのUI部分を分離

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@
 import styles from "./page.module.css";
 import { IconData } from "simple-icons/sdk";
 import { useEffect, useState } from "react";
-import { Button, Grid, Paper, Typography } from "@mui/material";
 import { randomInt } from "@/utils/commonUtil";
-import { createIconsUrl } from "@/utils/iconUtil";
+import GameUI from "@/components/GameUI";
 
 let iconSlugList: IconData[];
 const fetchSlugs = async () => {
@@ -13,9 +12,16 @@ const fetchSlugs = async () => {
   return data as IconData[];
 }
 
+const randomIcons = (iconList: IconData[], num: number) => {
+  const index_set = new Set<number>();
+  while (index_set.size < num) {
+    index_set.add(randomInt(0, iconList.length - 1));
+  }
+  const _iconList = Array.from(index_set).map(index => iconList[index]);
+  return _iconList;
+}
+
 export default function Home() {
-  const [attention, setAttention] = useState<number>(0)
-  const [correct, setCorrect] = useState<boolean>(false);
   const [correctIcon, setCorrectIcon] = useState<IconData>();
   const [iconList, setIconList] = useState<IconData[]>([]);
   useEffect(() => {
@@ -26,66 +32,15 @@ export default function Home() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const iconClick = (icon: IconData) => {
-    if (correct) {
-      return;
-    }
-
-    if (icon?.title == correctIcon?.title) {
-      alert("Correct");
-      setCorrect(true);
-    }
-    else {
-      alert(`Incorrect!\nCorrect answer is: ${correctIcon?.title}\nYour answer is: ${icon?.title}`);
-      setAttention(attention + 1);
-    }
-  }
-
   const onNextClick = () => {
-    setAttention(0);
-    let _iconList: IconData[] = [];
-    for (let i = 0; i < 12; i++) {
-      /// 重複しないようにする
-      while (true) {
-        const _icon = iconSlugList[randomInt(0, iconSlugList.length - 1)];
-        if (_icon.title == correctIcon?.title) {
-          continue;
-        }
-        _iconList.push(_icon);
-        break;
-      }
-    }
+    const _iconList = randomIcons(iconSlugList, 12);
     setIconList(_iconList);
     setCorrectIcon(_iconList[randomInt(0, _iconList.length - 1)]);
-    setCorrect(false);
   }
 
   return (
     <main className={styles.main}>
-      <Typography variant="h4" component="h4" >
-        Yomi-fuda : {correctIcon?.title}
-      </Typography>
-      <Typography variant="h6" component="h6" >
-        Number of touches: {attention}
-      </Typography>
-      <Button variant="contained" onClick={onNextClick} disabled={!correct}>
-        Next
-      </Button>
-      <Grid container spacing={{ xs: 2, md: 3 }} rowSpacing={{ xs: 1, sm: 2, md: 3 }} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        {iconList.map((icon, index) => {
-          return (
-            <Grid item key={index} xs={6} sm={4} md={3} lg={3} xl={2}>
-              <Paper elevation={3}
-                onClick={() => iconClick(icon)}
-                component="img"
-                alt=""
-                src={createIconsUrl(icon, 2)}
-                sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-              />
-            </Grid>
-          );
-        })}
-      </Grid>
+      <GameUI correctIcon={correctIcon} iconList={iconList} onNextGame={onNextClick} />
     </main>
   );
 }
---
+++ src/components/GameUI.tsx
@@ -0,0 +1,69 @@
+import { IconData } from "simple-icons/sdk";
+import { useState } from "react";
+import { Button, Grid, Paper, Typography } from "@mui/material";
+import { createIconsUrl } from "@/utils/iconUtil";
+type Props = {
+    correctIcon: IconData | undefined;
+    iconList: IconData[];
+    onNextGame: () => void;
+}
+
+const GameUI = (props: Props) => {
+    const [attention, setAttention] = useState<number>(0);
+    const [correct, setCorrect] = useState<boolean>(false);
+
+
+    const { correctIcon, iconList, onNextGame } = props;
+
+    const iconClick = (icon: IconData) => {
+        if (correct) {
+            return;
+        }
+
+        if (icon?.title == correctIcon?.title) {
+            alert("Correct");
+            setCorrect(true);
+        }
+        else {
+            alert(`Incorrect!\nCorrect answer is: ${correctIcon?.title}\nYour answer is: ${icon?.title}`);
+            setAttention(attention + 1);
+        }
+    }
+
+    const _onNextClick = () => {
+        setAttention(0);
+        onNextGame();
+        setCorrect(false);
+    }
+
+    return (
+        <>
+            <Typography variant="h4" component="h4" >
+                Yomi-fuda : {correctIcon?.title}
+            </Typography>
+            <Typography variant="h6" component="h6" >
+                Number of touches: {attention}
+            </Typography>
+            <Button variant="contained" onClick={_onNextClick} disabled={!correct}>
+                Next
+            </Button>
+            <Grid container spacing={{ xs: 2, md: 3 }} rowSpacing={{ xs: 1, sm: 2, md: 3 }} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                {iconList.map((icon, index) => {
+                    return (
+                        <Grid item key={index} xs={6} sm={4} md={3} lg={3} xl={2}>
+                            <Paper elevation={3}
+                                onClick={() => iconClick(icon)}
+                                component="img"
+                                alt=""
+                                src={createIconsUrl(icon, 0)}
+                                sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                            />
+                        </Grid>
+                    );
+                })}
+            </Grid>
+        </>
+    );
+}
+
+export default GameUI
